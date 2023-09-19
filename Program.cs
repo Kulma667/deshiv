ace6a8f0efb12d7e34933ffe0057066361b64d09
Kulma667 <112530908+Kulma667@users.noreply.github.com>
2023-09-19 23:53:43 +0600

    just done
    just done

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Coder_Decoder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = ""; // ввод
+            string output = ""; // шрифованный 
+            string decodedOutput = ""; // дешиф
+            input = Console.ReadLine(); // ввод
+            output = Convert.ToBase64String(Encoding.UTF8.GetBytes(input)); // шифруем
+            Console.WriteLine("decoded: "+output); // выводим
+            decodedOutput = Encoding.UTF8.GetString(Convert.FromBase64String(output));// дешифруем
+            Console.WriteLine("DE-decoded: "+decodedOutput); //выводим
+            Console.ReadLine();
+        }
+    }
+}
